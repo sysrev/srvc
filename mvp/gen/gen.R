670fbbf9c3ffa6f0f93287415481addfea196367
Thomas Luechtefeld <tom@insilica.co>
2022-06-02 10:59:05 -0400

    remove the R mvp

--- mvp/gen/gen.R
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env Rscript
-fifo   <- commandArgs(trailingOnly=TRUE)
-purrr::walk(1:100,~ write(sprintf("{i:%s}\n",.),file=fifo))
--- mvp/map/rev.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-fifo   <- commandArgs(trailingOnly=TRUE)
-input  <- fifo(fifo[[1]],open="r")
-output <- fifo[[2]]
-repeat{
-  line <- readLines(input,n=1)
-  if(identical(line,character(0))){break}
-  write(line,"\n")
-}
