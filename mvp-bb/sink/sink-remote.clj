0ce18b640bbf81237ee9fb5396f44a608ce68847
John Shaffer <jshaffer2112@protonmail.com>
2022-06-20 16:28:36 -0500

    Stream data to the remote
    Currently it is showing all-or-nothing behavior

--- mvp-bb/sink/sink-remote.clj
+++ mvp-bb/sink/sink-remote.clj
@@ -18,35 +18,34 @@
        (map json/read-str)
        (into #{})))
 
-(defn write-item [item writer existing]
-  (when-not (existing (:hash item))
-    (json/write item writer)
-    (.write writer "\n")
-    (.flush writer)))
-
-(let [[config-file infile] *command-line-args*
-      {:keys [db labels]} (json/read-str (slurp config-file) :key-fn keyword)]
+(defn upload-lines! [remote lines]
   (with-open [os (java.io.PipedOutputStream.)]
-    (let [writer (io/writer os)
-          existing (atom (existing-hashes db))
+    (let [is (java.io.PipedInputStream. os)
+          writer (io/writer os)
           upload (future
-                   (curl/post (api-route db "upload")
-                              {:body (java.io.PipedInputStream. os)}))]
-      (doseq [label labels]
-        (let [label (hash/add-hash {:data label :type "label"})]
-          (write-item label writer @existing)
-          (swap! existing conj (:hash label))))
-      (doseq [line (-> infile io/reader line-seq)
-              :let [{:keys [hash] :as m} (json/read-str line :key-fn keyword)
-                    actual-hash (hash/hash m)]]
-        (if (not= actual-hash hash)
-          (throw (ex-info "Hash mismatch" {:actual-hash actual-hash :expected-hash hash :item m}))
-          (when-not (@existing hash)
-            (.write writer line)
-            (.write writer "\n")
-            (.flush writer)
-            (swap! existing conj hash))))
+                   (curl/post (api-route remote "upload")
+                              {:body is}))]
+      (doseq [line lines]
+        (.write writer line)
+        (.write writer "\n"))
+      (.flush writer)
       (.close os)
       @upload)))
+
+(let [[config-file infile] *command-line-args*
+      {:keys [db labels]} (json/read-str (slurp config-file) :key-fn keyword)
+      label-items (map #(hash/add-hash {:data % :type "label"}) labels)
+      existing (atom (existing-hashes db))
+      new-labels (remove (comp @existing :hash) label-items)]
+  (upload-lines! db (map json/write-str new-labels))
+  (swap! existing into (map :hash new-labels))
+  (doseq [line (-> infile io/reader line-seq)
+          :let [{:keys [hash] :as m} (json/read-str line :key-fn keyword)
+                actual-hash (hash/hash m)]]
+    (if (not= actual-hash hash)
+      (throw (ex-info "Hash mismatch" {:actual-hash actual-hash :expected-hash hash :item m}))
+      (when-not (@existing hash)
+        (upload-lines! db [line])
+        (swap! existing conj hash)))))
 
 nil
