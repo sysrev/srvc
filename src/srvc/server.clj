23eca74230c6e2a91310f69ada558662a07c6fdc
John Shaffer <john@insilica.co>
2022-08-18 16:20:26 -0500

    Use donut.system to load config and manage state

--- src/srvc/server.clj
+++ src/srvc/server.clj
@@ -2,7 +2,10 @@
   (:refer-clojure :exclude [hash])
   (:require [babashka.process :as p]
             [clojure.data.json :as json]
+            [clojure.edn :as edn]
             [clojure.java.io :as io]
+            [clojure.string :as str]
+            [donut.system :as ds]
             [hiccup.core :as h]
             [lambdaisland.uri :as uri]
             [org.httpkit.server :as server]
@@ -199,19 +202,19 @@
    :headers {"Content-Type" "application/json"}
    :body "{\"success\":true}"})
 
-(defn review [request review-port]
+(defn review [request proxy-host]
   (response
    (body
     request
     [:iframe {:class "w-full bg-white"
               :style "height: 90vh"
-              :src (str "http://127.0.0.1:" review-port)}])))
+              :src proxy-host}])))
 
-(defn routes [dtm data-file review-port]
+(defn routes [dtm data-file proxy-host]
   (into
    [["/" {:get #(articles % dtm)}]
     ["/activity" {:get #(activity % dtm)}]
-    ["/review" {:get #(review % review-port)}]
+    ["/review" {:get #(review % proxy-host)}]
     ["/hx"
      ["/activity" {:get #(hx-activity % dtm)}]]
     ["/api/v1"
@@ -235,7 +238,7 @@
      {:not-found not-found})
     :path "/"}))
 
-(defonce state (atom {}))
+(defonce state (atom nil))
 
 (defn handle-tail-line [dtm db http-port line]
   (let [{:keys [data type] :as event} (json/read-str line :key-fn keyword)]
@@ -244,35 +247,104 @@
         (deliver http-port (:http-port data)))
       (add-events! dtm db [event]))))
 
+(defn first-line [s]
+  (some-> s not-empty (str/split #"\n") first))
+
+(defn signal! [system signal-name]
+  (let [{out ::ds/out :as system} (ds/signal system signal-name)
+        {:keys [error validation]} out]
+    (cond
+      (seq error)
+      (throw (ex-info
+              (str "Error during " signal-name
+                   (some->> error :services first val :message
+                            first-line (str ": ")))
+              out))
+
+      (seq validation)
+      (throw (ex-info
+              (str "Validation failed during " signal-name
+                   (some->> validation :services first val :message
+                            first-line (str ": ")))
+              out))
+
+      :else system)))
+
+(defn get-config [filename]
+  (if-let [resource (io/resource filename)]
+    (with-open [reader (-> resource io/reader java.io.PushbackReader.)]
+      (try
+        (edn/read reader)
+        (catch Exception e
+          (throw
+           (ex-info (str "Error parsing EDN in config file \"" filename
+                         \" ": " (.getMessage e))
+                    {:filename filename}
+                    e)))))
+    (throw
+     (ex-info (str "Config file not found: \"" filename "\"")
+              {:filename filename}))))
+
+(def config-component
+  #::ds{:start (fn [{::ds/keys [config]}]
+                 (get-config (:filename config)))
+        :stop (constantly nil)
+        :config {:filename (ds/local-ref [:env :config-file])}})
+
+(def dtm-component
+  #::ds{:start (fn [{::ds/keys [config]}]
+                 (atom (load-data (:db config))))
+        :stop (constantly nil)
+        :config {:db (ds/local-ref [:sr-yaml :db])}})
+
+(def http-server-component
+  #::ds{:start (fn [{{:keys [db dtm proxy-host]} ::ds/config}]
+                 (let [server (server/run-server
+                               (-> (routes dtm db proxy-host)
+                                   rr/router
+                                   (rr/ring-handler
+                                    (default-handler)
+                                    {:middleware [wrap-session]}))
+                               {:legacy-return-value? false})]
+                   {:port (server/server-port server)
+                    :server server}))
+        :stop (fn [{::ds/keys [instance]}]
+                @(server/server-stop! (:server instance))
+                nil)
+        :config {:db (ds/local-ref [:sr-yaml :db])
+                 :dtm (ds/local-ref [:dtm])
+                 :host (ds/local-ref [:config :host])
+                 :port (ds/local-ref [:config :port])
+                 :proxy-host (ds/local-ref [:config :proxy-host])
+                 :proxy-port (ds/local-ref [:config :proxy-port])
+                 :saml (ds/local-ref [:config :saml])}})
+
+(def sr-yaml-component
+  #::ds{:start (fn [{::ds/keys [config]}]
+                 (review/load-config (:filename config)))
+        :stop (constantly nil)
+        :config {:filename (ds/local-ref [:env :sr-yaml-file])}})
+
+(defn system [env]
+  {::ds/defs
+   {:srvc-server
+    {:env {::ds/start (constantly env)}
+     :config config-component
+     :dtm dtm-component
+     :http-server http-server-component
+     :sr-yaml sr-yaml-component}}})
+
 ;; Not thread-safe. For use by -main and at REPL
 (defn start! [flow-name]
-  (let [{:keys [db] :as config} (review/load-config "sr.yaml")
-        dtm (atom (load-data db))
-        http-port (promise)
-        review (review/review-process config flow-name
-                                      (partial handle-tail-line dtm db http-port)
-                                      prn)
-        proxy-port 8091
-        http-port (or (deref http-port 30000 nil)
-                      (throw (Exception. "Timed out waiting on http-port from review process")))
-        review-proxy (review/proxy-server http-port proxy-port)
-        server (server/run-server (-> (routes dtm db proxy-port)
-                                      rr/router
-                                      (rr/ring-handler
-                                       (default-handler)
-                                       {:middleware [wrap-session]})))]
-    (->> {:review review
-          :review-proxy review-proxy
-          :server server}
-         (reset! state))))
+  (let [env {:config-file "srvc-server-config.edn"
+             :flow-name flow-name
+             :sr-yaml-file "sr.yaml"}]
+    (swap! state #(signal! (or % (system env)) ::ds/start))))
 
 ;; Not thread-safe. For use by -main and at REPL
 #_:clj-kondo/ignore
 (defn stop! []
-  (let [{:keys [review server]} @state]
-    (server)
-    (p/destroy-tree review)
-    (reset! state nil)))
+  (swap! state #(signal! % ::ds/stop)))
 
 (defn -main [flow-name]
   (start! flow-name)
